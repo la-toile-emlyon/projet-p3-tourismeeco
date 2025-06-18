2bd3a4d052f9bf4deb9946766f9df298a246b369
Assia456 <assiaidali456@gmail.com>
2025-06-18 11:08:53 +0200

    Avis client terminer

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const header = document.querySelector("header")
 const hero = document.querySelector(".sectionHero")
+const heroDoc = document.querySelector(".heroDoc")
 
 window.addEventListener("scroll",()=>{
     if(window.scrollY > hero.clientHeight - 64){
@@ -8,3 +9,12 @@ window.addEventListener("scroll",()=>{
         header.classList.remove("coloredHeader")
     }
 })
+
+window.addEventListener("scroll",()=>{
+    if(window.scrollY > heroDoc.clientHeight - 64){
+        header.classList.add("coloredHeaderDoc")
+    }else{
+        header.classList.remove("coloredHeader")
+    }
+})
+
